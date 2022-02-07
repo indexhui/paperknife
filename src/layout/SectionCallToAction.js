cb34a56e2acd4b9536fc1f3e96bdc2243033e049
hugh <indexhui@gmail.com>
2022-02-08 03:30:35 +0800

    fix cta position

--- src/layout/SectionCallToAction.js
+++ src/layout/SectionCallToAction.js
@@ -18,7 +18,12 @@ const delayTransition = { duration: 0.5, type: 'tween', delay: '.05' };
 
 const SectionCallToAction = () => {
   return (
-    <Flex pt={{ base: '10px', lg: '50px' }} pb={{ base: '40px', lg: '80px' }}>
+    <Flex
+      pt={{ base: '10px', lg: '50px' }}
+      pb={{ base: '40px', lg: '80px' }}
+      w="100%"
+      justify="center"
+    >
       <Flex direction="column">
         <MotionBox
           initial="hidden"
